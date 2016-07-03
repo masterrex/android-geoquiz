807690d9cee23ef059a6e4788ae04a749aecbcec
Rex Hardin <masterrex@outlook.com>
2016-07-03 15:10:26 -0500

    Chapter 2 - Complete

--- app/src/main/java/io/retinal/geoquiz/QuizActivity.java
+++ app/src/main/java/io/retinal/geoquiz/QuizActivity.java
@@ -5,19 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
-
     private int mCurrentIndex = 0;
 
     private Question[] mQuestionBank = new Question[] {
@@ -27,6 +30,20 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_africa, false),
             new Question(R.string.question_asia, true)
     };
+    private void nextQuestion(){
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        mPrevButton.setEnabled(true);
+        updateQuestion();
+    }
+    private void prevQuestion(){
+        mCurrentIndex--;
+        if (mCurrentIndex <= 0) {
+            mCurrentIndex = 0;
+            mPrevButton.setEnabled(false);
+        }
+        // Log.v(TAG, "mCurrentIndex="+mCurrentIndex);
+        updateQuestion();
+    }
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -39,6 +56,7 @@ public class QuizActivity extends AppCompatActivity {
             responseToast = R.string.incorrect_toast;
         }
         Toast.makeText(QuizActivity.this, responseToast, Toast.LENGTH_SHORT).show();
+        nextQuestion();
     }
 
     @Override
@@ -47,14 +65,18 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextQuestion();
+            }
+        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
-
             }
         });
 
@@ -66,15 +88,25 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
-
+                nextQuestion();
             }
         });
+
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prevQuestion();
+            }
+        });
+        mPrevButton.setEnabled(false);
+
+        updateQuestion();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
