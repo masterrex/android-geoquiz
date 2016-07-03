ebdf56f59dfb4df861620e267c1a98e673d1022a
Rex Hardin <masterrex@outlook.com>
2016-07-03 14:11:15 -0500

    Commit... page 51

---
+++ app/src/main/java/io/retinal/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package io.retinal.geoquiz;
+
+/**
+ * Created by rex on 7/2/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question (int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
